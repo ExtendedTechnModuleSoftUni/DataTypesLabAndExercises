85c535f80b423a3eada77d30d733ba7f9701aa1d
VenciSarov <venci.sarov@gmail.com>
2017-02-04 00:31:07 +0200

    Solution added.

---
+++ DataTypesAllTasks/03E. Distance of The Stars/DistanceOfTheStars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class DistanceOfTheStars
+{
+    static void Main()
+    {
+        decimal distanceEarthNearestStar = 4.22M * 9450000000000;
+        decimal distanceGalaxyCenter = 26000M * 9450000000000;
+        decimal diameter = 100000M * 9450000000000;
+        decimal earthUniverse = 46500000000M * 9450000000000;
+
+        Console.WriteLine("{0:f2}", distanceEarthNearestStar);
+        Console.WriteLine("{0:f2}", distanceGalaxyCenter);
+        Console.WriteLine("{0:f2}", diameter);
+        Console.WriteLine("{0:f2}", earthUniverse);
+    }
+}
+
