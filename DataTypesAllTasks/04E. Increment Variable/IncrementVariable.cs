4ff845dc34a7f9c57bf9c81832b7a721421cb661
VenciSarov <venci.sarov@gmail.com>
2017-02-04 00:31:27 +0200

    Solution added.

---
+++ DataTypesAllTasks/04E. Increment Variable/IncrementVariable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class IncrementVariable
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        byte byteValue = 0;
+        byte overflowCounter = 0;
+        bool isOverflow = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            byteValue++;
+
+            if (byteValue == 0)
+            {
+                overflowCounter++;
+                byteValue = 0;
+                isOverflow = true;
+            }
+        }
+
+        if (isOverflow)
+        {
+            Console.WriteLine($"{byteValue}\nOverflowed {overflowCounter} times");
+        }
+        else
+        {
+            Console.WriteLine(byteValue);
+        }
+    }
+}
+
