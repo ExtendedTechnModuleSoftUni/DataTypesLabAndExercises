e5211cedd099bfa3b0eaf0bd8a26aea1f36e44c7
VenciSarov <venci.sarov@gmail.com>
2017-02-04 00:33:12 +0200

    Solution added.

---
+++ DataTypesAllTasks/05E. From Terabytes to Bits/FromTerabytesToBits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class FromTerabytesToBits
+{
+    static void Main()
+    {
+        double teraBytes = double.Parse(Console.ReadLine());
+
+        decimal bits = (decimal)(teraBytes * (Math.Pow(1024.0, 4) * 8));
+
+        Console.WriteLine(bits);
+    }
+}
+
