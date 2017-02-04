462822597a9ee6b03ee2c0d9a4e717a645c35113
VenciSarov <venci.sarov@gmail.com>
2017-02-04 16:49:55 +0200

    new task and solution added.

---
+++ DataTypesAllTasks/09E. PracticeChractersAndString/PracticeChractersAndString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class PracticeChractersAndString
+{
+    static void Main()
+    {
+        string textLine1 = "Software University";
+        char symbolB = 'B';
+        char symbolY = 'y';
+        char symbolE = 'e';
+        string textLine2 = "I love programming";
+
+        Console.WriteLine($"{textLine1}\n{symbolB}\n{symbolY}\n{symbolE}\n{textLine2}");
+    }
+}
+
