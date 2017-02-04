e86371bbb0feb44d6298e7ca893fe62cfc37ba90
VenciSarov <venci.sarov@gmail.com>
2017-02-04 22:24:49 +0200

    New task and solution added. Needs to be checked in Judge system!!!

---
+++ DataTypesAllTasks/15E. Cypher Roulette/CypherRoulette.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CypherRoulette
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        RollingText(n);
+    }
+
+    static void RollingText(int n)
+    {
+        int spinCounter = 0;
+        List<string> text = new List<string>();
+        string[] arr = new string[2 * n];
+        int start = 0;
+        for (int i = 0; i < n; i++, start++)
+        {
+            string wordOrCommand = Console.ReadLine();
+
+            if (wordOrCommand != "spin" && spinCounter % 2 == 0)
+            {
+                if (text.Count > 0)
+                {
+                    if (text[text.Count - 1] == (wordOrCommand))
+                    {
+                        text.Clear();
+                    }
+                    else
+                    {
+                        text.Add(wordOrCommand);
+                    }
+                }
+                else
+                {
+                    text.Add(wordOrCommand);
+                }
+            }
+            else if (wordOrCommand == "spin")
+            {
+                arr[start] = wordOrCommand;
+
+                if (start > 1)
+                {
+                    if (arr[start] == arr[start - 1])
+                    {
+                        text.Clear();
+                    }
+                }            
+
+                spinCounter++;
+                i--;
+            }
+            else
+            {
+                text.Insert(0, wordOrCommand);
+            }
+        }
+
+        Console.WriteLine(string.Join("", text));
+    }
+}
